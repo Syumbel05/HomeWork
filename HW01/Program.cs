a70650c976582f1360ecd19563526ea0742ddfeb
syumbel <syu.kashapova@mail.ru>
2022-10-28 18:47:40 +0500

    сделала дз2

---
+++ HW01/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA=5;
+int numberB=7;
+int Max=0 ;
+int Min=0;
+
+        if (numberA > numberB)
+            {
+             Console.WriteLine(Max=numberA);
+             Console.WriteLine(Min=numberB);
+            }
+        else
+         {
+                Console.WriteLine(Max=numberB);
+                Console.WriteLine(Min=numberA);
+         }
+System.Console.WriteLine($"Число {numberA} Меньшее,  Число {numberB} Большее");
